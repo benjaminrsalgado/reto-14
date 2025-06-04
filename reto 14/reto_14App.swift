491c1bfca15370d6ede4cca537245b58dac7f254
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-06-04 10:54:43 -0600

    Initial Commit

---
+++ reto 14/reto_14App.swift
@@ -0,0 +1,17 @@
+//
+//  reto_14App.swift
+//  reto 14
+//
+//  Created by Benjamin Rojo on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct reto_14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
