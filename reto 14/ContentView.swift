33b0928006c63c58f77110426ef92d3a89c1d300
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-06-04 13:44:49 -0600

    i practice the @State with buttons

--- reto 14/ContentView.swift
+++ reto 14/ContentView.swift
@@ -14,11 +14,26 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            LightSwitchView()
         }
         .padding()
     }
 }
 
+struct LightSwitchView: View{
+    @State private var light = false
+    var body: some View{
+        Button (action:{
+            light.toggle()
+        }){
+            Image(systemName: "lightbulb.fill")
+                .foregroundColor(light ? .yellow: .white)
+        }
+        Text(light ? "¡Luz encendida!" : "Luz apagada")
+            .font(.headline)
+            .padding(.top, 10)
+    }
+}
 #Preview {
     ContentView()
 }
